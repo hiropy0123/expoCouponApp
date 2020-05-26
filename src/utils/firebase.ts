60d6c1123dab801ce8fbc3679b4abf02cfd4efea
Hiroki Nakashima <hiroki123naka4ma@gmail.com>
2020-05-26 17:46:54 +0900

    add firebase

---
+++ src/utils/firebase.ts
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDVQtCVjjgJcbTp7LrW0JBmE7dJ7E8iTzg',
+  authDomain: 'couponapp-bb967.firebaseapp.com',
+  databaseURL: 'https://couponapp-bb967.firebaseio.com',
+  projectId: 'couponapp-bb967',
+  storageBucket: 'couponapp-bb967.appspot.com',
+  messagingSenderId: '352994825275',
+  appId: '1:352994825275:web:64bf480abc34e08614781e',
+};
+
+firebase.initializeApp(firebaseConfig);
